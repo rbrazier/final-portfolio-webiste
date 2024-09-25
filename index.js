b6ae00216744faeb11887ba58be57736a4080745
rbrazier <19rbrazier@thelangton.org.uk>
2024-09-25 12:09:44 +0100

    adding my js file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ while (password=" "){
     password=prompt("Enter password to view my portoflio - hint the letter code on the comp sci door","");
     if (password=pass1){
         alert("correct password, click ok to enter");
-        break
     }
     else{
         window.location="https://info.knowbe4.com/cybersecurity/awareness";
